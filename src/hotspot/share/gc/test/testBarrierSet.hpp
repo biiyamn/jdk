c8e9a9f9081dfd17422e535c1894cac8b95a344c
biiyamn <aymen.laabidi@gmail.com>
2025-05-02 10:51:11 +0200

    [NO-JDK-ISSUE] Add TestGC sources based on EpsilonGC
    Introduce a new experimental garbage collector TestGC by duplicating and renaming EpsilonGC sources.
    All class and symbol names have been adapted (EpsilonHeap → TestHeap, etc.) in preparation for integration.
    
    This GC will later be decoupled from the JVM and loaded as a shared library for external GC experimentation.

---
+++ src/hotspot/share/gc/test/testBarrierSet.hpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2017, 2018, Red Hat, Inc. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+#ifndef SHARE_GC_TEST_TESTBARRIERSET_HPP
+#define SHARE_GC_TEST_TESTBARRIERSET_HPP
+
+#include "gc/shared/barrierSet.hpp"
+
+// No interaction with application is required for Epsilon, and therefore
+// the barrier set is empty.
+class TestBarrierSet: public BarrierSet {
+  friend class VMStructs;
+
+public:
+  TestBarrierSet();
+
+  virtual void print_on(outputStream *st) const {}
+
+  virtual void on_thread_create(Thread* thread);
+  virtual void on_thread_destroy(Thread* thread);
+
+  template <DecoratorSet decorators, typename BarrierSetT = TestBarrierSet>
+  class AccessBarrier: public BarrierSet::AccessBarrier<decorators, BarrierSetT> {};
+};
+
+template<>
+struct BarrierSet::GetName<TestBarrierSet> {
+  static const BarrierSet::Name value = BarrierSet::TestBarrierSet;
+};
+
+template<>
+struct BarrierSet::GetType<BarrierSet::TestBarrierSet> {
+  typedef ::TestBarrierSet type;
+};
+
+#endif // SHARE_GC_TEST_TESTBARRIERSET_HPP
